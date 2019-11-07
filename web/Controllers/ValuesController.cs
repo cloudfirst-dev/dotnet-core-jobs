34d0226f4e851d60fdb377b7d7a10f6461224edd
Joshua Mathianas <mathianasj@idsysapps.com>
2019-11-07 08:26:29 -0500

    add first sample of running prebuilt job

--- web/Controllers/ValuesController.cs
+++ web/Controllers/ValuesController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using k8s;
+using k8s.Models;
+using System.Collections;
 
 namespace web.Controllers
 {
@@ -14,7 +17,58 @@ namespace web.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            KubernetesClientConfiguration config;
+            
+            // determine where to load config from
+            if (string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("KUBERNETES_SERVICE_HOST"))) {
+                config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+            } else {
+                config = KubernetesClientConfiguration.InClusterConfig();
+            }
+             
+            var client = new Kubernetes(config);
+            var list = client.ListNamespacedPod("dot-net-jobs-dev");
+            var pod = list.Items[0];
+            var pods = new List<String>();
+
+            foreach (var item in list.Items)
+            {
+                pods.Add(item.Metadata.Name);
+            }
+
+            var job = new V1Job
+            {
+                Metadata = new V1ObjectMeta {
+                    Name = "test-" + Guid.NewGuid()
+                },
+                Spec = new V1JobSpec {
+                    Template = new V1PodTemplateSpec {
+                        Metadata = new V1ObjectMeta {
+                            Name = "runner"
+                        },
+                        Spec = new V1PodSpec {
+                            Containers = new List<V1Container> {
+                                new V1Container {
+                                    Name = "runner",
+                                    Image = "centos:7",
+                                    Command = new List<string> {
+                                        "bin/bash",
+                                        "-c",
+                                        "for i in 9 8 7 6 5 4 3 2 1 ; do echo $i ; done"
+                                    }
+                                }
+                            },
+                            RestartPolicy = "Never"
+                        }
+                    }
+                }
+            };
+
+            var result = client.CreateNamespacedJob(job, "dot-net-jobs-dev");
+
+            Console.WriteLine(result);
+
+            return pods.ToArray();
         }
 
         // GET api/values/5
